93c4b0aa0432fb2b9b2647681dda2d01b5b1407d
sucupirap <pedro41julio@gmail.com>
2025-12-27 21:07:08 -0300

    BST

---
+++ bst.py
@@ -0,0 +1,38 @@
+from node import Node
+
+class BynarySearchTree:
+
+    def __init__(self):
+        self.root = None # Árvore está inicialmente vazia quando criada
+    
+    def insert(self, key, data):
+        self.root = self._insert_recursive(self.root, key, data)
+    
+    def _insert_recursive(self, node, key, data):
+
+        if node is None:
+            return Node(key, data)
+        
+        if key < node.key :
+            node.left = self._insert_recursive(node.left, key, data)
+        
+        elif key > node.key :
+            node.right = self._insert_recursive(node.right, key, data)
+        
+        return None
+    
+    def search(self, key):
+
+        return self._search_recursive(self.root, key)
+    
+    def _search_recursive(self, node, key):
+
+        if node is None or node.key == key : # retorna caso o vertice encontrado seja o escolhido
+            return node
+        
+        if key < node.key : #vasculhar a arvore buscando o valor de acordo com o tamanho da chave
+            return self._search_recursive(node.left, key)
+        
+        return self._search_recursive(node.right, key)
+
+        
